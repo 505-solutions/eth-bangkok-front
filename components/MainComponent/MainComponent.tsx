04e744fa60ab7f27e983d0fe201dba2e9f451255
Gal Gantar <gantar.gal@gmail.com>
2024-11-16 23:20:45 +0700

    Added Pol in TEE

--- components/MainComponent/MainComponent.tsx
+++ components/MainComponent/MainComponent.tsx
@@ -1,7 +1,7 @@
-import { Button, Center, Code, Card, Text, JsonInput, Group, FileButton, Space, Table, Avatar, ThemeIcon, Badge, Image } from '@mantine/core';
+import { Button, Center, Code, Card, Text, JsonInput, Group, FileButton, Space, Table, Avatar, ThemeIcon, Badge, Image, Title } from '@mantine/core';
 // import { Prism } from '@mantine/prism';
 import { Tabs, rem } from '@mantine/core';
-import { IconBoxModel, IconChartArrows, IconLoadBalancer, IconBrandGoogle, IconShield, IconMap, IconMoneybag, IconDatabase, IconBrandPython, IconGitCompare, IconUser } from '@tabler/icons-react';
+import { IconBoxModel, IconChartArrows, IconLoadBalancer, IconBrandGoogle, IconShield, IconMap, IconMoneybag, IconDatabase, IconBrandPython, IconGitCompare, IconUser, IconCrossFilled, IconCancel, IconFileTypeDoc, IconSignLeftFilled, IconContract } from '@tabler/icons-react';
 import {
     DynamicContextProvider,
     DynamicWidget,
@@ -100,15 +100,9 @@ export function MainComponent() {
 
                     <Card mt={20} mb={20} shadow="sm" padding="lg" radius="md" withBorder style={{ width: 'min-content' }}>
                         <Card.Section>
-                            {/* <Image
-                              src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
-                              height={160}
-                              alt="Norway"
-                            /> */}
                             <Center>
                                 <ThemeIcon
                                   variant="gradient"
-                                // size="xl"
                                   size={70}
                                   aria-label="Gradient action icon"
                                   gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
@@ -143,15 +137,9 @@ export function MainComponent() {
 
                     <Card ml={20} mt={20} mb={20} shadow="sm" padding="lg" radius="md" withBorder style={{ width: 'min-content' }}>
                         <Card.Section>
-                            {/* <Image
-                              src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
-                              height={160}
-                              alt="Norway"
-                            /> */}
                             <Center>
                                 <ThemeIcon
                                   variant="gradient"
-                                // size="xl"
                                   size={70}
                                   aria-label="Gradient action icon"
                                   gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
@@ -171,13 +159,14 @@ export function MainComponent() {
                             Download the composite dataset made up of users contributions.
                         </Text>
 
-                        <Code>cifar-10-composite.tar.gz</Code>
+                        <Code>cifar10-composite.tar.gz</Code>
 
                         <Button
                           onClick={() => handleDownload(
                                 'https://raw.githubusercontent.com/505-solutions/phala-tee-python/refs/heads/main/model.py',
                                 'model.py'
                             )}
+                            mb={20}
                             >
                                 Download dataset
                                 <IconDatabase style={{ marginLeft: '8px' }} />
@@ -185,55 +174,125 @@ export function MainComponent() {
                     </Card>
 
                     <Card mt={20} ml={20} mb={20} mr={20} shadow="sm" padding="lg" radius="md" withBorder>
-                        Train the model by running the following command in the shell:
-                        <Code>python model.py</Code>
+                        <Title order={3}>Train the model</Title>
+                        <Text>
+                            First prepare <Code style={{display: 'contents'}}>python3.10</Code> environment with the following libraries:
+                        </Text>
+                        <Code block>
+                            {`numpy==1.26.4
+torch==1.11.0
+scipy==1.10.0`}
+                        </Code>
+                        <Text mt={30}>
+                            Train the model by running the following command in the shell:
+                        </Text>
+                        <Code>python model.py --save-freq=1000</Code>
                     </Card>
                 </Tabs.Panel>
 
-                <Tabs.Panel value="verify-training">
-                    <Center><p>Upload final model and proof</p></Center>
-                    <Group>
-                        <Group justify="center">
-                            <FileButton resetRef={resetRef1} onChange={setFile1} accept="image/png,image/jpeg">
-                            {(props) => <Button {...props}>Upload trained model</Button>}
-                            </FileButton>
+                <Tabs.Panel value="verify-training" style={{display: 'flex'}}>
+                    <Card mt={20} mb={20} mr={20} shadow="sm" padding="lg" radius="md" withBorder style={{ width: 'min-content' }}>
+                        <Card.Section>
+                            <Center>
+                                <ThemeIcon
+                                  variant="gradient"
+                                  size={70}
+                                  aria-label="Gradient action icon"
+                                  gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
+                                  mt={20}
+                                        >
+                                        <IconBrandPython size={60} />
+                                </ThemeIcon>
+                            </Center>
+                        </Card.Section>
+
+                        <Group justify="space-between" mt="md" mb="xs">
+                            <Text fw={500}>Upload trained model</Text>
+                            <Badge color="pink">Python</Badge>
+                        </Group>
+
+                        <Text size="sm" c="dimmed" mb={20}>
+                            Once the model is done training, upload it here to verify the training in TEE.
+                        </Text>
+                        <Group>
+                            <Group justify="center">
+                                <FileButton resetRef={resetRef1} onChange={setFile1}>
+                                {(props) => <Button {...props}>
+                                    Upload trained model
+                                    <IconBrandPython style={{ marginLeft: '8px' }} />
+                                </Button>}
+                                </FileButton>
+                            </Group>
+                            {file1 && (
+                                <Text size="sm" ta="center" mt="sm">
+                                Picked file: <Code>{file1.name}</Code>
+                                </Text>
+                            )}
                             <Button disabled={!file1} color="red" onClick={clearFile1}>
-                            Reset
+                                Reset
+                                <IconCancel style={{ marginLeft: '8px' }} />
                             </Button>
                         </Group>
-                        {file1 && (
-                            <Text size="sm" ta="center" mt="sm">
-                            Picked file: {file1.name}
-                            </Text>
-                        )}
-                    </Group>
-                    <Group>
-                        <Group justify="center">
-                            <FileButton resetRef={resetRef2} onChange={setFile2} accept="image/png,image/jpeg">
-                            {(props) => <Button {...props}>Upload proof of learning</Button>}
-                            </FileButton>
-                            <Button disabled={!file2} color="red" onClick={clearFile2}>
-                            Reset
+                    </Card>
+                    <Card mt={20} mb={20} mr={20} shadow="sm" padding="lg" radius="md" withBorder style={{ width: 'min-content' }}>
+                        <Card.Section>
+                            <Center>
+                                <ThemeIcon
+                                  variant="gradient"
+                                  size={70}
+                                  aria-label="Gradient action icon"
+                                  gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
+                                  mt={20}
+                                        >
+                                        <IconContract size={60} />
+                                </ThemeIcon>
+                            </Center>
+                        </Card.Section>
+
+                        <Group justify="space-between" mt="md" mb="xs">
+                            <Text fw={500}>Upload proof of learning (PoL)</Text>
+                            <Badge color="pink">Proof of learning</Badge>
+                        </Group>
+
+                        <Text size="sm" c="dimmed" mb={20}>
+                            <b>The proof of learning (PoL)</b> proves that the model was trained correctly on the dataset.
+                        </Text>
+                        <Group>
+                            <Group justify="center">
+                                <FileButton resetRef={resetRef1} onChange={setFile1}>
+                                {(props) => <Button {...props}>
+                                    Upload proof of learning
+                                    <IconContract style={{ marginLeft: '8px' }} />
+                                </Button>}
+                                </FileButton>
+                            </Group>
+                            {file1 && (
+                                <Text size="sm" ta="center" mt="sm">
+                                Picked file: <Code>{file1.name}</Code>
+                                </Text>
+                            )}
+                            <Button disabled={!file1} color="red" onClick={clearFile1}>
+                                Reset
+                                <IconCancel style={{ marginLeft: '8px' }} />
                             </Button>
                         </Group>
-                        {file2 && (
-                            <Text size="sm" ta="center" mt="sm">
-                            Picked file: {file2.name}
-                            </Text>
-                        )}
-                    </Group>
-                    <Space h={20} />
+                    </Card>
 
-                    <Button
-                      onClick={handleTeeVerification}
-                    >
-                        Verify proof of learning in TEE
-                    </Button>
+                    <Card mt={20} mb={20} shadow="sm" padding="lg" radius="md" withBorder style={{width: 'max-content'}}>
+                        <Title order={3}>Verify proof of learning</Title>
+                        <p>We verify proof of learning inside Phala networks TEE</p>
 
-                    { quote &&
-                    <p>Tdx quote: {quote.slice(0, 20)}...{quote.slice(-5)}</p>
-                    }
-                    <Button>Aggregate TDX quote</Button>
+                        <Button
+                        onClick={handleTeeVerification}
+                        >
+                            Verify proof of learning in TEE
+                        </Button>
+
+                        { quote &&
+                        <p>Tdx quote: {quote.slice(0, 20)}...{quote.slice(-5)}</p>
+                        }
+                        <Button>Aggregate TDX quote</Button>
+                    </Card>
 
                 </Tabs.Panel>
 
