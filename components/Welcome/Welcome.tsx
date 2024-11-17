42ec97948e6e2e98dc16efda0249247d78178d56
Gal Gantar <gantar.gal@gmail.com>
2024-11-17 07:44:56 +0700

    Added backend disclaimer

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,4 +1,4 @@
-import { Anchor, Text, Title } from '@mantine/core';
+import { Anchor, Center, Text, Title } from '@mantine/core';
 import classes from './Welcome.module.css';
 
 export function Welcome() {
@@ -12,6 +12,8 @@ export function Welcome() {
       <Text ta="center" size="lg" maw={580} mx="auto" mt="xl">
         <Anchor href="https://github.com/cleverhans-lab/Proof-of-Learning" size="lg"><b>Proof of learning</b></Anchor> AI traing that uses <Anchor href="https://filecoin.io" size="lg"><b>Filecoin</b></Anchor> for dataset storage, <Anchor href="https://phala.network" size="lg"><b>Phala TEE</b></Anchor> for proof verification and <Anchor href="https://flare.network" size="lg"><b>Flare Network</b></Anchor> for data collection and price feeds. 
       </Text>
+
+      <Center><Text>INFO: the project requires local backend</Text></Center>
     </>
   );
 }
