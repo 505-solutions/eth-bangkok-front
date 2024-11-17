b0fcc83918efb3806447d873573b4fccaeaa34bb
Gal Gantar <gantar.gal@gmail.com>
2024-11-17 07:41:28 +0700

    Ignored types in build

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,13 @@ const withBundleAnalyzer = bundleAnalyzer({
 });
 
 export default withBundleAnalyzer({
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: false,
   eslint: {
     ignoreDuringBuilds: true,
